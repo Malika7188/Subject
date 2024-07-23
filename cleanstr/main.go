01528f33c0d7cb13bd75c6f0239010793126e2de
masman <asmanodeny@gmail.com>
2024-07-23 18:36:39 +0300

    exce

--- cleanstr/main.go
+++ cleanstr/main.go
@@ -9,29 +9,59 @@ func main() {
 	if len(os.Args) != 2 {
 		return
 	}
-	var result string
 	str := os.Args[1]
-	if len(str) == 0 {
-		fmt.Println()
-		return
-	}
-	arr := []string{}
-	for i, char := range str {
-		if char != ' ' {
-			result += string(char)
+	words := SplitS(str)
+	res := ""
+	for _, char := range words {
+		if char == " " {
+			res += string(char)
+		} else {
+			res += string(char) + " "
 		}
-		if (char == ' ' && result != "") || (i == len(str)-1 && result != "") {
-			arr = append(arr, result)
+	}
+	fmt.Println(res)
+}
+
+func SplitS(str string) []string {
+	result := ""
+	word := []string{}
+
+	for i := 0; i < len(str); i++ {
+		if str[i] != ' ' {
+			result += string(str[i])
+		} else if str[i] == ' ' && result != "" {
+			word = append(word, result)
 			result = ""
 		}
-
 	}
-	for i, v := range arr {
-		if i != len(arr)-1 {
-			fmt.Print(v + " ")
-		} else {
-			fmt.Print(v)
-		}
+	if result != "" {
+		word = append(word, result)
 	}
-	fmt.Println()
+	return word
 }
+
+// res := ""
+// str := os.Args[1]
+// if len(str) == 0 {
+// 	fmt.Println()
+// 	return
+// }
+// arr := []string{}
+// for i, char := range str {
+// 	if char != ' ' {
+// 		result += string(char)
+// 	}
+// 	if (char == ' ' && result != "") || (i == len(str)-1 && result != "") {
+// 		arr = append(arr, result)
+// 		result = ""
+// 	}
+
+// }
+// for i, v := range arr {
+// 	if i != len(arr)-1 {
+// 		fmt.Print(v + " ")
+// 	} else {
+// 		fmt.Print(v)
+// 	}
+// }
+// fmt.Println()

c996dd1c93773b993e3e0bff4d93215e5bc9860f
masman <asmanodeny@gmail.com>
2024-07-24 16:14:57 +0300

    from

---
+++ fromto/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	// "github.com/01-edu/z01"
+	"fmt"
+)
+
+func FromTo(from int, to int) string {
+	res := ""
+
+	if from < 0 || from > 99 || to < 0 || to > 99 {
+		return "invalid input\n"
+	}
+	if from < to {
+		for i := from; i <= to; i++ {
+			if i < 10 {
+				res += "0" + Itoa(i)
+			} else {
+				res += Itoa(i)
+			}
+			if i != to {
+				res += ", "
+			}
+		}
+	} else {
+		for i := from; i >= to; i-- {
+			if i < 10 {
+				res += "0" + Itoa(i)
+			} else {
+				res += Itoa(i)
+			}
+			if i != to {
+				res += ", "
+			}
+		}
+	}
+	return res + "\n"
+
+}
+func Itoa(num int) string {
+	neg := false
+	if num < 0 {
+		neg = true
+		num *= -1
+	}
+	res := ""
+	for num > 0 {
+		digit := num % 10
+		res = string(digit+'0') + res
+		num /= 10
+	}
+	if neg {
+		return "-" + res
+	}
+	return res
+}
+
+func main() {
+	fmt.Print(FromTo(1, 10))
+	fmt.Print(FromTo(10, 1))
+	fmt.Print(FromTo(10, 10))
+	fmt.Print(FromTo(100, 10))
+}

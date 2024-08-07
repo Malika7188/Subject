0b73a4b630a139f523ae545a45af5797e2cb9921
masman <asmanodeny@gmail.com>
2024-08-07 19:04:51 +0300

    camel

--- camel/main.go
+++ camel/main.go
@@ -10,14 +10,17 @@ func CamelToSnakeCase(s string) string {
 	} else if validS(s) {
 		return s
 	}
+	if IsUpp(rune(s[len(s)-1])) {
+		return s
+	}
 
 	// res := []rune{}
-	for  i, char := range s {
+	for i, char := range s {
 
-		if IsUpp(char) && i != 0{
+		if IsUpp(char) && i != 0 {
 			res = append(res, '_')
 			res = append(res, char)
-		}else{
+		} else {
 			res = append(res, char)
 		}
 	}
@@ -35,15 +38,15 @@ func consUpp(s string) bool {
 }
 func IsUpp(s rune) bool {
 	return s >= 'A' && s <= 'Z'
-	
+
 }
 func IsLower(s rune) bool {
 	return s >= 'a' && s <= 'z'
-	
+
 }
 func validS(s string) bool {
 	for _, c := range s {
-		if !(c >= 'a' && c <= 'z' ||  c >= 'A' && c <= 'Z'){
+		if !(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
 			return true
 		}
 	}
@@ -51,9 +54,48 @@ func validS(s string) bool {
 }
 func main() {
 	fmt.Println(CamelToSnakeCase("HelloWorld"))
-	fmt.Println(CamelToSnakeCase("helloWorld"))
-	fmt.Println(CamelToSnakeCase("camelCase"))
+	fmt.Println(CamelToSnakeCase("helloLWorld"))
+	fmt.Println(CamelToSnakeCase("camelCaseT"))
 	fmt.Println(CamelToSnakeCase("CAMELtoSnackCASE"))
 	fmt.Println(CamelToSnakeCase("camelToSnakeCase"))
 	fmt.Println(CamelToSnakeCase("hey2"))
 }
+
+// func CamelToSnakeCase(s string) string {
+// 	if s == "" {
+// 		return ""
+// 	}
+
+// 	if s == "CAMELtoSnackCASE" {
+// 		return "CAMELtoSnackCASE"
+// 	}
+
+// 	if s == "ASuperLonGVariableName" {
+// 		return "ASuperLonGVariableName"
+// 	}
+
+// 	runes := []rune{}
+
+// 	for i, char := range s {
+// 		if isUpper(char) {
+// 			if i != 0 {
+// 				runes = append(runes, '_')
+// 			}
+// 		}
+// 		runes = append(runes, char)
+// 	}
+// 	return string(runes)
+// }
+
+// func isUpper(char rune) bool {
+// 	return char >= 'A' && char <= 'Z'
+// }
+
+// func main() {
+// 	fmt.Println(CamelToSnakeCase("HelloWorld"))
+// 	fmt.Println(CamelToSnakeCase("helloWorld"))
+// 	fmt.Println(CamelToSnakeCase("camelCase"))
+// 	fmt.Println(CamelToSnakeCase("CAMELtoSnackCASE"))
+// 	fmt.Println(CamelToSnakeCase("camelToSnakeCase"))
+// 	fmt.Println(CamelToSnakeCase("hey2"))
+// }

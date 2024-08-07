d4a9033a1fcd1167896d29dfc3ac5c6b3a298bd9
masman <asmanodeny@gmail.com>
2024-08-07 19:04:18 +0300

    hscode

--- hashcode/main.go
+++ hashcode/main.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func HashCode(dec string) string {
-	hashed := make([]rune, len(dec))
+	// hashed := make([]rune, len(dec))
+	var hashed []rune
 	size := len(dec)
-	
-	for i, char := range dec {
-		hashvals := (int(char) + size)%127
-		
-		if hashvals < 32 || hashvals > 126 {
-			hashvals += 33
+	hash := 0
+
+	for _, char := range dec {
+		hash = (int(char) + size) % 127
+		if hash < 32 || hash > 126 {
+			hash += 33
 		}
-		hashed[i] = rune(hashvals)	
+		hashed = append(hashed, rune(hash))
 	}
 	return string(hashed)
 }
@@ -22,4 +23,5 @@ func main() {
 	fmt.Println(HashCode("AB"))
 	fmt.Println(HashCode("BAC"))
 	fmt.Println(HashCode("Hello World"))
+	fmt.Println(HashCode(" 14 Avril 1912"))
 }

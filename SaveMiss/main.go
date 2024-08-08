f02249b503ac5c28e6b07218a3a16545a99c5189
masman <asmanodeny@gmail.com>
2024-08-08 16:50:04 +0300

    sv

---
+++ SaveMiss/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func SaveAndMiss(arg string, num int) string {
+	res := ""
+	if num <= 0 {
+		return arg
+	}
+
+	for i, ch := range arg {
+		if (i/num)%2 == 0 {
+			res += string(ch)
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(SaveAndMiss("123456789", 3))
+	fmt.Println(SaveAndMiss("abcdefghijklmnopqrstuvwyz", 3))
+	fmt.Println(SaveAndMiss("", 3))
+	fmt.Println(SaveAndMiss("hello you all ! ", 0))
+	fmt.Println(SaveAndMiss("what is your name?", 0))
+	fmt.Println(SaveAndMiss("go Exercise Save and Miss", -5))
+}

f79e7298a59cdc60918d3735f2b4f62ab8bf730b
masman <asmanoden@gmail.com>
2024-08-15 17:30:06 +0300

    third

---
+++ thirdtime/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func ThirdTimeIsACharm(str string) string {
+	res := ""
+	for i := 2; i < len(str); i += 3 {
+		res += string(str[i])
+	}
+	return res + "\n"
+}
+
+func main() {
+	fmt.Print(ThirdTimeIsACharm("123456789"))
+	fmt.Print(ThirdTimeIsACharm(""))
+	fmt.Print(ThirdTimeIsACharm("a b c d e f"))
+	fmt.Print(ThirdTimeIsACharm("12"))
+}

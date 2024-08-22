7b0a9b3aa9f24c0ebc35dd653e5e54c37bf92743
masman <asmanoden@gmail.com>
2024-08-22 18:41:25 +0300

    modified slice

--- SLICE/main.go
+++ SLICE/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/lib/chars"
+	"github.com/01-edu/go-tests/lib/random"
+	"github.com/01-edu/go-tests/solutions"
+)
 
 func Slice(a []string, nbrs ...int) []string {
 	res := []string{}
@@ -15,7 +20,7 @@ func Slice(a []string, nbrs ...int) []string {
 	}
 	if len(nbrs) >= 2 {
 		num1 := nbrs[0]
-		num2 := nbrs[1] 
+		num2 := nbrs[1]
 
 		if num1 < 0 || num2 < 0 {
 			num1 += leng
@@ -32,10 +37,39 @@ func Slice(a []string, nbrs ...int) []string {
 }
 
 func main() {
-	a := []string{"coding", "algorithm", "ascii", "package", "golang"}
-	fmt.Printf("%#v\n", Slice(a, 1))
-	fmt.Printf("%#v\n", Slice(a, 2, 4))
-	fmt.Printf("%#v\n", Slice(a, -3))
-	fmt.Printf("%#v\n", Slice(a, -2, -1))
-	fmt.Printf("%#v\n", Slice(a, 2, 0))
+	elems := [][]interface{}{
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			1,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			-3,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			2, 4,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			-2, -1,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			2, 0,
+		},
+	}
+
+	s := random.StrSlice(chars.Words)
+
+	elems = append(elems, []interface{}{s, -len(s) - 10, -len(s) - 5})
+
+	for i := 0; i < 3; i++ {
+		s = random.StrSlice(chars.Words)
+		elems = append(elems, []interface{}{s, random.IntBetween(-len(s)-10, len(s)+10), random.IntBetween(-len(s)-8, len(s)+10)})
+	}
+
+	for _, a := range elems {
+		challenge.Function("Slice", Slice, solutions.Slice, a...)
+	}
 }

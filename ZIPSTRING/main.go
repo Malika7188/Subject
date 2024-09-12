3fc0ebaeee6e7772aae698c54613a7ea3d99396f
masman <asmanoden@gmail.com>
2024-09-12 16:43:56 +0300

    newfile

---
+++ ZIPSTRING/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ZipString(s string) string {
+	count := 1
+	res := ""
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			count++
+		} else {
+			res += strconv.Itoa(count) + string(s[i-1])
+			count = 1
+		}
+	}
+	res += strconv.Itoa(count) + string(s[len(s)-1])
+	return res
+}
+func main() {
+	fmt.Println(ZipString("YouuungFellllas"))
+	fmt.Println(ZipString("Thee quuick browwn fox juumps over the laaazy dog"))
+	fmt.Println(ZipString("Helloo Therre!"))
+}
